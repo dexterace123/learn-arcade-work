cc6fea0e17ac64e17bc4b0a3f5d57e70c0ef73af
David Pritzkau <dexterace123@gmail.com>
2024-08-31 23:43:58 -0700

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,2 +1,4 @@
-test
-
+print("This is a test of Python.\nI am now escaping to the next line.")
+print("This is a third line of test, but second line of code.")
+print("Someday, I will create a Star Fleet Battles simulation.")
+print("Wouldn't that be fun?")
