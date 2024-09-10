06d42a8ca9c94cc5b81ae13ed8c0247a69bb7c02
David Pritzkau <dexterace123@gmail.com>
2024-09-09 23:04:26 -0700

    Lab 03

--- Lab 03 - Draw Using Functions/lab_03.py
+++ Lab 03 - Draw Using Functions/lab_03.py
@@ -7,43 +7,46 @@ import arcade
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 
-def draw_saucer_shape():
-    arcade.draw_circle_filled(210, 300, 200, (229, 228, 226))
-    arcade.draw_arc_outline(210, 300, 100, 100, arcade.color.BLACK, 90, 270)
+def draw_saucer_shape(x, y):
+    # Draw a point at x, y for reference
+    arcade.draw_point(x, y, arcade.color.RED, 5)
 
-def draw_main_body():
-    arcade.draw_lbwh_rectangle_filled(380, 265, 300, 70, (229, 228, 226))
-    arcade.draw_arc_filled(680, 300, 30, 70, (207, 196, 207), -90, 90)
+    arcade.draw_circle_filled(x, y, x - 10, (229, 228, 226))
+    arcade.draw_arc_outline(x, y, 100, 100, arcade.color.BLACK, 90, 270)
 
-def draw_nacelle_spokes():
-    arcade.draw_lbwh_rectangle_filled(570, 320, 30, 100, (229, 228, 226))
-    arcade.draw_lbwh_rectangle_filled(570, 180, 30, 100, (229, 228, 226))
+def draw_main_body(x, y):
+    arcade.draw_lbwh_rectangle_filled(x + 170, y - 35, 300, 70, (229, 228, 226))
+    arcade.draw_arc_filled(x + 470, y, 30, 70, (207, 196, 207), -90, 90)
 
-def draw_nacelles():
-    arcade.draw_lbwh_rectangle_filled(470, 420, 480, 40, (229, 228, 226))
-    arcade.draw_lbwh_rectangle_filled(470, 140, 480, 40, (228, 228, 226))
-    arcade.draw_arc_filled(470, 440, 40, 40, arcade.color.RED, 90, 270)
-    arcade.draw_arc_filled(470, 160, 40, 40, arcade.color.RED, 90, 270)
-    arcade.draw_arc_filled(950, 440, 20, 40, (207, 196, 207), -90, 90)
-    arcade.draw_arc_filled(950, 160, 20, 40, (207, 196, 207), -90, 90)
-    arcade.draw_lbwh_rectangle_filled(920, 420, 30, 40, (207, 196, 207))
-    arcade.draw_lbwh_rectangle_filled(920, 140, 30, 40, (207, 196, 207))
+def draw_nacelle_spokes(x, y):
+    arcade.draw_lbwh_rectangle_filled(x + 360, y + 20, 30, 100, (229, 228, 226))
+    arcade.draw_lbwh_rectangle_filled(x + 360, y - 120, 30, 100, (229, 228, 226))
 
-def draw_saucer_details():
-    arcade.draw_circle_outline(210, 300, 200, arcade.color.BLACK)
-    arcade.draw_circle_outline(210, 300, 175, arcade.color.BLACK)
-    arcade.draw_line(210, 250, 310, 300, arcade.color.BLACK)
-    arcade.draw_line(210, 350, 310, 300, arcade.color.BLACK)
-    arcade.draw_circle_outline(210, 300, 20, arcade.color.BLACK)
-    arcade.draw_text("U.S.S. ENTERPRISE", 130, 372, arcade.color.BLACK, 16, rotation=90)
-    arcade.draw_text("N", 110, 400, arcade.color.BLACK, 30, rotation=135)
-    arcade.draw_text("C", 85, 370, arcade.color.BLACK, 30, rotation=120)
-    arcade.draw_text("C", 70, 335, arcade.color.BLACK, 30, rotation=105)
-    arcade.draw_text("-", 65, 300, arcade.color.BLACK, 30, rotation=90)
-    arcade.draw_text("1", 65, 275, arcade.color.BLACK, 30, rotation=79)
-    arcade.draw_text("7", 70, 250, arcade.color.BLACK, 30, rotation=68)
-    arcade.draw_text("0", 80, 225, arcade.color.BLACK, 30, rotation=57)
-    arcade.draw_text("1", 95, 200, arcade.color.BLACK, 30, rotation=45)
+def draw_nacelles(x, y):
+    arcade.draw_lbwh_rectangle_filled(x + 260, y + 120, 480, 40, (229, 228, 226))
+    arcade.draw_lbwh_rectangle_filled(x + 260, y - 160, 480, 40, (228, 228, 226))
+    arcade.draw_arc_filled(x + 260, y + 140, 40, 40, arcade.color.RED, 90, 270)
+    arcade.draw_arc_filled(x + 260, y - 140, 40, 40, arcade.color.RED, 90, 270)
+    arcade.draw_arc_filled(x + 740, y + 140, 20, 40, (207, 196, 207), -90, 90)
+    arcade.draw_arc_filled(x + 740, y - 140, 20, 40, (207, 196, 207), -90, 90)
+    arcade.draw_lbwh_rectangle_filled(x + 710, y + 120, 30, 40, (207, 196, 207))
+    arcade.draw_lbwh_rectangle_filled(x + 710, y - 160, 30, 40, (207, 196, 207))
+
+def draw_saucer_details(x, y):
+    arcade.draw_circle_outline(x, y, 200, arcade.color.BLACK)
+    arcade.draw_circle_outline(x, y, 175, arcade.color.BLACK)
+    arcade.draw_line(x, y - 50, x + 100, y, arcade.color.BLACK)
+    arcade.draw_line(x, y + 50, x + 100, y, arcade.color.BLACK)
+    arcade.draw_circle_outline(x, y, 20, arcade.color.BLACK)
+    arcade.draw_text("U.S.S. ENTERPRISE", x - 80, y + 72, arcade.color.BLACK, 16, rotation=90)
+    arcade.draw_text("N", x - 100, y + 100, arcade.color.BLACK, 30, rotation=135)
+    arcade.draw_text("C", x - 125, y + 70, arcade.color.BLACK, 30, rotation=120)
+    arcade.draw_text("C", x - 140, y + 35, arcade.color.BLACK, 30, rotation=105)
+    arcade.draw_text("-", x - 145, y, arcade.color.BLACK, 30, rotation=90)
+    arcade.draw_text("1", x - 145, y - 25, arcade.color.BLACK, 30, rotation=79)
+    arcade.draw_text("7", x - 140, y - 50, arcade.color.BLACK, 30, rotation=68)
+    arcade.draw_text("0", x - 130, y - 75, arcade.color.BLACK, 30, rotation=57)
+    arcade.draw_text("1", x - 115, y - 100, arcade.color.BLACK, 30, rotation=45)
 
 
 def main():
@@ -52,11 +55,13 @@ def main():
 
     arcade.start_render()
 
-    draw_saucer_shape()
-    draw_main_body()
-    draw_nacelle_spokes()
-    draw_nacelles()
-    draw_saucer_details()
+    x_center = 210
+    y_center = 300
+    draw_saucer_shape(x_center, y_center)
+    draw_main_body(x_center, y_center)
+    draw_nacelle_spokes(x_center, y_center)
+    draw_nacelles(x_center, y_center)
+    draw_saucer_details(x_center, y_center)
 
     arcade.finish_render()
     arcade.run()
